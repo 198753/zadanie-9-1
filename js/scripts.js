e5fb457e64c9b0fb2bdf62bd572408e8de354de8
Tomasz Andrzejewski <kasta.tomek@wp.pl>
2017-09-28 13:48:15 +0200

    W bólach zrodzona...

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(5, -7),
+	triangle3Area = getTriangleArea(10, 0);
+
+
+function getTriangleArea(a, h) {
+	if ((a <= 0) || (h <= 0)) {
+		console.log('Nieprawidłowe dane');
+	}
+	else {
+		return (a*h/2);
+	}
+}
+console.log(getTriangleArea(10, 6));
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
